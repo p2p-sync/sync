3e36eb591775a81ae3649557c3028b7d2c3a60f5
Raphael Matile <raphael.matile@gmail.com>
2016-01-13 11:53:28 +0100

    Add tests for bus events

--- src/main/java/org/rmatil/sync/core/eventbus/CreateBusEvent.java
+++ src/main/java/org/rmatil/sync/core/eventbus/CreateBusEvent.java
@@ -2,10 +2,20 @@ package org.rmatil.sync.core.eventbus;
 
 import org.rmatil.sync.event.aggregator.core.events.IEvent;
 
+/**
+ * An {@link IBusEvent} which indicates that a path has been
+ * created on disk and should be synchronized
+ */
 public class CreateBusEvent implements IBusEvent {
 
+    /**
+     * The event to propagate, generally a {@link org.rmatil.sync.event.aggregator.core.events.CreateEvent}
+     */
     protected IEvent event;
 
+    /**
+     * @param event The create event to propagate, generally a {@link org.rmatil.sync.event.aggregator.core.events.CreateEvent}
+     */
     public CreateBusEvent(IEvent event) {
         this.event = event;
     }
