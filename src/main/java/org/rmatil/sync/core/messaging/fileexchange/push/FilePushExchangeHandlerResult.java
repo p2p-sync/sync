9d345f635903ab3a06b3a13bdba92ebe16b75886
Raphael Matile <raphael.matile@gmail.com>
2016-01-14 17:07:39 +0100

    Add documentation and stub for FilePushExchangeHandlerResult

--- src/main/java/org/rmatil/sync/core/messaging/fileexchange/push/FilePushExchangeHandlerResult.java
+++ src/main/java/org/rmatil/sync/core/messaging/fileexchange/push/FilePushExchangeHandlerResult.java
@@ -1,5 +1,11 @@
 package org.rmatil.sync.core.messaging.fileexchange.push;
 
+/**
+ * Use this object to specify a result after a file push
+ * is complete
+ *
+ * @see FilePushRequest
+ */
 public class FilePushExchangeHandlerResult {
 
 }
