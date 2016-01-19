c852aacdadee6333bcbbb52366da64f06c45ec99
Raphael Matile <raphael.matile@gmail.com>
2016-01-19 20:17:16 +0100

    Add string length comparator

---
+++ src/main/java/org/rmatil/sync/core/StringLengthComparator.java
@@ -0,0 +1,28 @@
+package org.rmatil.sync.core;
+
+import java.util.Comparator;
+
+/**
+ * Compares two strings by their length.
+ */
+public class StringLengthComparator implements Comparator<String> {
+
+    /**
+     * Compares the strings by their length.
+     * <br>
+     * <br>
+     * {@inheritDoc}
+     */
+    @Override
+    public int compare(String o1, String o2) {
+        if (o1.length() < o2.length()) {
+            return - 1;
+        }
+
+        if (o1.length() > o2.length()) {
+            return 1;
+        }
+
+        return 0;
+    }
+}
