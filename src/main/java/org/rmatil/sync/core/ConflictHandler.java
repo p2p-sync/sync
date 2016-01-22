b50e91cdc2881052a44b8356506d3c51ef0a6cda
Raphael Matile <raphael.matile@gmail.com>
2016-01-22 23:27:08 +0100

    Add NonBlockingBackgroundSyncer

---
+++ src/main/java/org/rmatil/sync/core/ConflictHandler.java
@@ -0,0 +1,93 @@
+package org.rmatil.sync.core;
+
+import net.engio.mbassy.bus.MBassador;
+import org.rmatil.sync.commons.path.Naming;
+import org.rmatil.sync.core.eventbus.CreateBusEvent;
+import org.rmatil.sync.core.eventbus.IBusEvent;
+import org.rmatil.sync.core.eventbus.IgnoreBusEvent;
+import org.rmatil.sync.event.aggregator.core.events.CreateEvent;
+import org.rmatil.sync.event.aggregator.core.events.MoveEvent;
+import org.rmatil.sync.persistence.api.IFileMetaInfo;
+import org.rmatil.sync.persistence.api.IPathElement;
+import org.rmatil.sync.persistence.api.IStorageAdapter;
+import org.rmatil.sync.persistence.api.StorageType;
+import org.rmatil.sync.persistence.core.local.LocalPathElement;
+import org.rmatil.sync.persistence.exceptions.InputOutputException;
+import org.rmatil.sync.version.api.IObjectStore;
+import org.rmatil.sync.version.core.model.PathObject;
+import org.rmatil.sync.version.core.model.Version;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * A utility class to handle conflict files
+ */
+public class ConflictHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(ConflictHandler.class);
+
+    /**
+     * Creates a conflict file for the given path element.
+     * Note, that an ignore event for the move of the local conflict file is emitted to the event bus.
+     * Additionally, a create event for the conflict file is emitted to the event bus.
+     *
+     * @param globalEventBus      The global event bus to publish the new conflict file event
+     * @param conflictFilePostfix The postfix to append to the filename (must be unique over all clients)
+     * @param objectStore         The object store to modify when creating the conflict file
+     * @param storageAdapter      The storage adapter to access the path element
+     * @param pathElement         The path element for which to create a conflict file
+     */
+    public static Path createConflictFile(MBassador<IBusEvent> globalEventBus, String conflictFilePostfix, IObjectStore objectStore, IStorageAdapter storageAdapter, IPathElement pathElement) {
+        PathObject pathObject;
+        try {
+            pathObject = objectStore.getObjectManager().getObjectForPath(pathElement.getPath());
+        } catch (InputOutputException e) {
+            logger.error("Failed to check file versions of file " + pathElement.getPath() + ". Message: " + e.getMessage() + ". Indicating that a conflict happened");
+            return null;
+        }
+
+        // compare local and remote file versions
+        List<Version> localFileVersions = pathObject.getVersions();
+        Version lastLocalFileVersion = localFileVersions.size() > 0 ? localFileVersions.get(localFileVersions.size() - 1) : null;
+        String lastLocalFileVersionHash = (null != lastLocalFileVersion) ? lastLocalFileVersion.getHash() : null;
+
+        Path conflictFilePath;
+        try {
+            IFileMetaInfo fileMetaInfo = storageAdapter.getMetaInformation(pathElement);
+            conflictFilePath = Paths.get(Naming.getConflictFileName(pathElement.getPath(), true, fileMetaInfo.getFileExtension(), conflictFilePostfix));
+            globalEventBus.publish(new IgnoreBusEvent(
+                    new MoveEvent(
+                            Paths.get(pathElement.getPath()),
+                            conflictFilePath,
+                            conflictFilePath.getFileName().toString(),
+                            lastLocalFileVersionHash,
+                            System.currentTimeMillis()
+                    )
+            ));
+            globalEventBus.publish(new CreateBusEvent(
+                    new CreateEvent(
+                            conflictFilePath,
+                            conflictFilePath.getFileName().toString(),
+                            lastLocalFileVersionHash,
+                            System.currentTimeMillis()
+                    )
+            ));
+
+        } catch (InputOutputException e) {
+            logger.error("Can not read meta information for file " + pathElement.getPath() + ". Moving the conflict file failed");
+            return null;
+        }
+
+        try {
+            storageAdapter.move(StorageType.FILE, pathElement, new LocalPathElement(conflictFilePath.toString()));
+        } catch (InputOutputException e) {
+            logger.error("Can not move conflict file " + pathElement.getPath() + " to " + conflictFilePath.toString() + ". Message: " + e.getMessage());
+        }
+
+        return conflictFilePath;
+    }
+}
---
+++ src/main/java/org/rmatil/sync/core/syncer/background/NonBlockingBackgroundSyncer.java
@@ -0,0 +1,293 @@
+package org.rmatil.sync.core.syncer.background;
+
+import net.engio.mbassy.bus.MBassador;
+import org.rmatil.sync.core.ConflictHandler;
+import org.rmatil.sync.core.StringLengthComparator;
+import org.rmatil.sync.core.Zip;
+import org.rmatil.sync.core.eventbus.CreateBusEvent;
+import org.rmatil.sync.core.eventbus.IBusEvent;
+import org.rmatil.sync.core.eventbus.IgnoreBusEvent;
+import org.rmatil.sync.core.messaging.fileexchange.demand.FileDemandExchangeHandler;
+import org.rmatil.sync.core.syncer.background.fetchobjectstore.FetchObjectStoreExchangeHandler;
+import org.rmatil.sync.core.syncer.background.fetchobjectstore.FetchObjectStoreExchangeHandlerResult;
+import org.rmatil.sync.event.aggregator.api.IEventAggregator;
+import org.rmatil.sync.event.aggregator.core.events.DeleteEvent;
+import org.rmatil.sync.event.aggregator.core.events.ModifyEvent;
+import org.rmatil.sync.network.api.IClient;
+import org.rmatil.sync.network.api.IClientManager;
+import org.rmatil.sync.network.core.model.ClientDevice;
+import org.rmatil.sync.network.core.model.ClientLocation;
+import org.rmatil.sync.persistence.api.IPathElement;
+import org.rmatil.sync.persistence.api.IStorageAdapter;
+import org.rmatil.sync.persistence.api.StorageType;
+import org.rmatil.sync.persistence.core.local.LocalPathElement;
+import org.rmatil.sync.persistence.core.local.LocalStorageAdapter;
+import org.rmatil.sync.version.api.IObjectStore;
+import org.rmatil.sync.version.api.PathType;
+import org.rmatil.sync.version.core.ObjectStore;
+import org.rmatil.sync.version.core.model.PathObject;
+import org.rmatil.sync.version.core.model.Version;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class NonBlockingBackgroundSyncer implements IBackgroundSyncer {
+
+    private static final Logger logger = LoggerFactory.getLogger(NonBlockingBackgroundSyncer.class);
+
+    protected IEventAggregator eventAggregator;
+
+    protected IClient client;
+
+    protected IClientManager clientManager;
+
+    protected IObjectStore objectStore;
+
+    protected IStorageAdapter storageAdapter;
+
+    protected MBassador<IBusEvent> globalEventBus;
+
+    public NonBlockingBackgroundSyncer(IEventAggregator eventAggregator, IClient client, IClientManager clientManager, IObjectStore objectStore, IStorageAdapter storageAdapter, MBassador<IBusEvent> globalEventBus) {
+        this.eventAggregator = eventAggregator;
+        this.client = client;
+        this.clientManager = clientManager;
+        this.objectStore = objectStore;
+        this.storageAdapter = storageAdapter;
+        this.globalEventBus = globalEventBus;
+    }
+
+    @Override
+    public void run() {
+        try {
+            UUID exchangeId = UUID.randomUUID();
+            logger.info("Starting non blocking background syncer (Exchange: " + exchangeId + ")");
+            this.eventAggregator.stop();
+
+            FetchObjectStoreExchangeHandler fetchObjectStoreExchangeHandler = new FetchObjectStoreExchangeHandler(
+                    this.client,
+                    this.clientManager,
+                    exchangeId
+            );
+
+            Thread fetchObjectStoreExchangeHandlerThread = new Thread(fetchObjectStoreExchangeHandler);
+            fetchObjectStoreExchangeHandlerThread.setName("FetchObjectStoreExchangeHandler-" + exchangeId);
+            fetchObjectStoreExchangeHandlerThread.start();
+
+            logger.info("Waiting for fetching of object stores to complete");
+
+            try {
+                fetchObjectStoreExchangeHandler.await();
+            } catch (InterruptedException e) {
+                logger.error("Got interrupted while waiting for fetching all object stores");
+            }
+
+            if (! fetchObjectStoreExchangeHandler.isCompleted()) {
+                logger.error("FetchObjectStoreExchangeHandler should be completed after awaiting. Since we do not know about the other clients object store, we abort background sync for exchange " + exchangeId);
+                return;
+            }
+
+            FetchObjectStoreExchangeHandlerResult result = fetchObjectStoreExchangeHandler.getResult();
+
+            Map<ClientDevice, IObjectStore> objectStores = Zip.unzipObjectStore(this.objectStore, result);
+
+            // use a tree map for guaranteed ordering
+            Map<String, ClientDevice> deletedPaths = new TreeMap<>(new StringLengthComparator());
+            Map<String, ClientDevice> updatedPaths = new TreeMap<>(new StringLengthComparator());
+            Map<String, ClientDevice> conflictPaths = new TreeMap<>(new StringLengthComparator());
+
+            for (Map.Entry<ClientDevice, IObjectStore> entry : objectStores.entrySet()) {
+                HashMap<ObjectStore.MergedObjectType, Set<String>> outdatedOrDeletedPaths = this.objectStore.mergeObjectStore(entry.getValue());
+
+                for (String outDatedPath : outdatedOrDeletedPaths.get(ObjectStore.MergedObjectType.CHANGED)) {
+                    updatedPaths.put(outDatedPath, entry.getKey());
+                }
+
+                for (String deletedPath : outdatedOrDeletedPaths.get(ObjectStore.MergedObjectType.DELETED)) {
+                    deletedPaths.put(deletedPath, entry.getKey());
+                }
+
+                for (String conflictPath : outdatedOrDeletedPaths.get(ObjectStore.MergedObjectType.CONFLICT)) {
+                    conflictPaths.put(conflictPath, entry.getKey());
+                }
+
+                entry.getValue().getObjectManager().getStorageAdapater().delete(new LocalPathElement("./"));
+                this.objectStore.getObjectManager().getStorageAdapater().delete(new LocalPathElement(entry.getKey().getClientDeviceId().toString()));
+            }
+
+            // delete all removed files
+            logger.info("Removing all (" + deletedPaths.size() + ") deleted files");
+            for (Map.Entry<String, ClientDevice> entry : deletedPaths.entrySet()) {
+                logger.debug("Removing deleted path " + entry.getKey());
+                this.storageAdapter.delete(new LocalPathElement(entry.getKey()));
+            }
+
+            logger.info("Creating all (" + conflictPaths.size() + ") conflict files");
+            for (Map.Entry<String, ClientDevice> entry: conflictPaths.entrySet()) {
+                logger.debug("Creating conflict file " + entry.getKey());
+                Path conflictFilePath = ConflictHandler.createConflictFile(
+                        this.globalEventBus,
+                        this.client.getClientDeviceId().toString(),
+                        this.objectStore,
+                        this.storageAdapter,
+                        new LocalPathElement(entry.getKey())
+                );
+
+
+
+                // we have to emit an ignore event here for the file syncer
+                // otherwise the final difference calculation will get an updated
+                // path (i.e. actually an create event) and will try to sync it to the other clients
+                if (null != conflictFilePath) {
+                    this.globalEventBus.publish(new IgnoreBusEvent(
+                            new ModifyEvent(
+                                    conflictFilePath,
+                                    conflictFilePath.getFileName().toString(),
+                                    "weIgnoreTheHash",
+                                    System.currentTimeMillis()
+                            )
+                    ));
+                }
+
+                // now add this to the updated paths, so that we can fetch the original again
+                updatedPaths.put(entry.getKey(), entry.getValue());
+            }
+
+            // fetch all missing files
+            logger.info("Fetching all (" + updatedPaths.size() + ") missing files");
+
+            for (Map.Entry<String, ClientDevice> entry : updatedPaths.entrySet()) {
+                UUID subExchangeId = UUID.randomUUID();
+                logger.debug("Starting to fetch file " + entry.getKey() + " with subExchangeId " + subExchangeId + " (non blocking background sync " + exchangeId + ")");
+
+                // before updating, check the actual content hash on disk
+                // to prevent data loss during sync
+                PathObject mergedPathObject = this.objectStore.getObjectManager().getObjectForPath(entry.getKey());
+                Version lastVersion = mergedPathObject.getVersions().get(Math.max(0, mergedPathObject.getVersions().size() - 1));
+
+                PathType pathType = mergedPathObject.getPathType();
+                StorageType storageType = pathType.equals(PathType.DIRECTORY) ? StorageType.DIRECTORY : StorageType.FILE;
+
+                // only check version, if the file does exist on our disk,
+                // if not, we have to fetch it anyway
+                if (this.storageAdapter.exists(storageType, new LocalPathElement(mergedPathObject.getAbsolutePath()))) {
+                    this.objectStore.syncFile(this.storageAdapter.getRootDir().resolve(mergedPathObject.getAbsolutePath()).toFile());
+                    PathObject modifiedPathObject = this.objectStore.getObjectManager().getObjectForPath(entry.getKey());
+                    Version modifiedLastVersion = modifiedPathObject.getVersions().get(Math.max(0, modifiedPathObject.getVersions().size() - 1));
+
+                    if (! modifiedLastVersion.equals(lastVersion)) {
+                        // we just changed the file on this client while syncing...
+                        // therefore we use this state and do not request an outdated state from another client
+                        logger.info("Detected file change while merging object store (from other client or end user)... Using our state");
+                        continue;
+                    }
+                }
+
+                FileDemandExchangeHandler fileDemandExchangeHandler = new FileDemandExchangeHandler(
+                        this.storageAdapter,
+                        this.client,
+                        this.clientManager,
+                        new ClientLocation(
+                                entry.getValue().getClientDeviceId(),
+                                entry.getValue().getPeerAddress()
+                        ),
+                        entry.getKey(),
+                        subExchangeId
+                );
+
+                this.client.getObjectDataReplyHandler().addResponseCallbackHandler(subExchangeId, fileDemandExchangeHandler);
+
+                Thread fileDemandExchangeHandlerThread = new Thread(fileDemandExchangeHandler);
+                fileDemandExchangeHandlerThread.setName("FileDemandExchangeHandlerThread-" + subExchangeId);
+                fileDemandExchangeHandlerThread.start();
+
+                try {
+                    fileDemandExchangeHandler.await();
+                } catch (InterruptedException e) {
+                    logger.error("Got interrupted while waiting for fileDemandExchangeHandler " + subExchangeId + " to complete. Message: " + e.getMessage());
+                }
+
+                this.client.getObjectDataReplyHandler().removeResponseCallbackHandler(subExchangeId);
+
+                if (! fileDemandExchangeHandler.isCompleted()) {
+                    logger.error("FileDemandExchangeHandler " + subExchangeId + " should be completed after wait.");
+                }
+            }
+
+            // start event aggregator
+            logger.info("Starting event aggregator on client (" + this.client.getPeerAddress().inetAddress().getHostName() + ":" + this.client.getPeerAddress().tcpPort() + "): Non-blocking background sync " + exchangeId);
+            this.eventAggregator.start();
+
+            logger.info("Reconciling local disk changes with merged object store (non-blocking background sync " + exchangeId + ")");
+            // create a temporary second object store to get changes made in the mean time of syncing
+            IStorageAdapter objectStoreStorageManager = this.objectStore.getObjectManager().getStorageAdapater();
+            IPathElement pathElement = new LocalPathElement("nonBlockingBackgroundSyncObjectStore");
+            if (objectStoreStorageManager.exists(StorageType.DIRECTORY, pathElement)) {
+                objectStoreStorageManager.delete(pathElement);
+            }
+
+            objectStoreStorageManager.persist(StorageType.DIRECTORY, pathElement, null);
+
+            // create the temporary object store in the .sync folder
+            Path rootPath = this.storageAdapter.getRootDir();
+            IStorageAdapter changeObjectStoreStorageManager = new LocalStorageAdapter(objectStoreStorageManager.getRootDir().resolve(pathElement.getPath()));
+            IObjectStore changeObjectStore = new ObjectStore(rootPath, "index.json", "object", changeObjectStoreStorageManager);
+
+            // build object store for differences in the mean time
+            List<String> ignoredPaths = new ArrayList<>();
+            Path origSyncFolder = this.objectStore.getObjectManager().getStorageAdapater().getRootDir().getFileName();
+            ignoredPaths.add(origSyncFolder.toString());
+            changeObjectStore.sync(rootPath.toFile(), ignoredPaths);
+
+            // get differences between disk and merged object store
+            HashMap<ObjectStore.MergedObjectType, Set<String>> updatedOrDeletedPaths = this.objectStore.mergeObjectStore(changeObjectStore);
+
+            // remove change object store again
+            changeObjectStoreStorageManager.delete(new LocalPathElement("./"));
+
+            Set<String> deletedPathsInTheMeanTime = updatedOrDeletedPaths.get(ObjectStore.MergedObjectType.DELETED);
+            Set<String> updatedPathsInTheMeanTime = updatedOrDeletedPaths.get(ObjectStore.MergedObjectType.CHANGED);
+
+            logger.info("Found " + deletedPathsInTheMeanTime.size() + " paths which have been deleted in the mean time of syncing");
+            for (String deletedPath : deletedPathsInTheMeanTime) {
+                // publish a delete event to the SyncFileChangeListener
+                logger.trace("Creating delete event for " + deletedPath);
+                this.globalEventBus.publish(
+                        new CreateBusEvent(
+                                new DeleteEvent(
+                                        Paths.get(deletedPath),
+                                        Paths.get(deletedPath).getFileName().toString(),
+                                        null,
+                                        System.currentTimeMillis()
+                                )
+                        )
+                );
+            }
+
+            logger.info("Found " + updatedPathsInTheMeanTime.size() + " paths which have changed in the mean time of syncing");
+            for (String updatedPath : updatedPathsInTheMeanTime) {
+                // publish modify events to SyncFileChangeListener
+                logger.trace("Creating modify event for " + updatedPath);
+                PathObject updatedPathObject = this.objectStore.getObjectManager().getObjectForPath(updatedPath);
+
+                this.globalEventBus.publish(
+                        new CreateBusEvent(
+                                new ModifyEvent(
+                                        Paths.get(updatedPath),
+                                        Paths.get(updatedPath).getFileName().toString(),
+                                        updatedPathObject.getVersions().get(Math.max(0, updatedPathObject.getVersions().size() - 1)).getHash(),
+                                        System.currentTimeMillis()
+                                )
+                        )
+                );
+            }
+
+            logger.info("Completed non-blocking background sync " + exchangeId);
+
+        } catch (Exception e) {
+            logger.error("Got exception in NonBlockingBackgroundSyncer. Message: " + e.getMessage(), e);
+        }
+    }
+}
